3bc2595a1732a2bcfaf938aa17d998bdd40ade29
panchao <web_panchao@163.com>
2021-04-19 13:46:51 +0800

    1

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,19 +1,23 @@
 <!--
  * @Author: your name
  * @Date: 2021-04-19 09:47:42
- * @LastEditTime: 2021-04-19 09:50:21
+ * @LastEditTime: 2021-04-19 13:46:26
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vite-project\src\views\home.vue
 -->
 <template>
-  home
+  <div>
+    <p>home</p>
+    <el-button>btn</el-button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+import {ElButton} from 'element-plus'
 export default defineComponent({
   name: 'home',
+  components:{ElButton}
 })
 </script>
